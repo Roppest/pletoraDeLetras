661416c0d792eb0ad01a100ffd54c777edcf8dc8
Rodrigo Vazquez <rodvaudio@gmail.com>
2019-07-20 19:59:46 -0500

    Update Servidor.java

--- Servidor.java
+++ Servidor.java
@@ -5,16 +5,22 @@ public class Servidor
 {
   public static void main(String[] args) throws IOException
   {
+    int solicitudes = 0;
     ServerSocket serverSocket = new ServerSocket(8081);
-    System.out.println("Servidor iniciado en espera de peticion...")
-    Socket socket = serverSocket.accept();
+    System.out.println("Servidor iniciado en: " + serverSocket.getInetAddress() +
+    " puerto: " + serverSocket.getLocalPort() + "\nEscuchado...");
+    while(solicitudes < 5)
+    {
+      Socket socket = serverSocket.accept();
+      InputStreamReader in = new InputStreamReader(socket.getInputStream());
+      BufferedReader buff = new BufferedReader(in);
 
-    System.out.println("Cliente conectado");
+      String str = buff.readLine();
+      System.out.println("Recibido: " + str);
+      solicitudes++;
+    }
 
-    InputStreamReader in = new InputStreamReader(socket.getInputStream());
-    BufferedReader buff = new BufferedReader(in);
 
-    String str = buff.readLine();
-    System.out.println("Recibido: " + str);
+    serverSocket.close();
   }
 }
