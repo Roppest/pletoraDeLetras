c8f0f4cc5da052f1933467e52214a5cb205c0bd2
Rodrigo Vazquez <rodvaudio@gmail.com>
2019-07-21 19:45:19 -0500

    se cambio a paquete lector.

--- Servidor.java
+++ Servidor.java
@@ -1,8 +1,21 @@
 /**
-  *Ejecucion: java -cp WEB-INF/classes/xalan.jar Servidor
+  *Ejecucion: java -cp .:jdom.jar: lector.Servidor 
   */
+package lector;
 import java.net.*;
 import java.io.*;
+import java.io.FileWriter;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.Attribute;
+import org.jdom.Namespace;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.XMLOutputter;
+import org.jdom.output.Format;
+import org.jdom.JDOMException;
+
+
 
 public class Servidor
 {
@@ -13,7 +26,9 @@ public class Servidor
     System.out.println("Servidor iniciado en: " + serverSocket.getInetAddress() +
     " puerto: " + serverSocket.getLocalPort() + "\nEscuchado...");
     String xml = "peticionCliente.xml";//ruta del xml que formara
-    ValidadorXML validador = new ValidadorXML();
+    String xsd = "lector/xsd/solicitud.xsd";
+    XMLValidator validador = new XMLValidator();
+    LectorXML lector = new LectorXML();
     while(solicitudes < 5)
     {
       Socket socket = serverSocket.accept();
@@ -26,14 +41,35 @@ public class Servidor
 
       //transformamos la cadena en un documento xml
       java.io.FileWriter fw = new java.io.FileWriter(xml);
+
       fw.write(str);
       fw.close();
-
-      try
+      if(validador.validate(xml,xsd))
       {
-        System.out.println("XML valido: "+ validador.validaXML(xml));
+        try
+        {
+          SAXBuilder constructor = new SAXBuilder();
+          Document doc = constructor.build(xml);
 
-      }catch(Exception e){System.out.println(e);}
+          Element raiz = doc.getRootElement();
+          int servicio = Integer.parseInt(raiz.getChild("servicio").getText());
+
+          switch(servicio)
+          {
+            case 1: //mostrar libros x anio
+              //List<Element> lista = lector.obtenerLibros(String anio);
+              System.out.println("servicio1");
+            break;
+            case 2: //mostrar libro x isbn
+            break;
+            default:
+            break;
+          }
+        }catch(JDOMException e){e.printStackTrace();}
+      }
+      else
+      System.out.println("XML no valido. Cerrando comunicacion con cliente.");
+
 
       solicitudes++;
     }
